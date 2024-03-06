a98fac612b26444920b9f3a07fc426fecb27047d
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-06 01:17:30 -0500

    chore: use bun for pm2 interpreter

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	name: "auv", // Name of your application
 	script: "src/index.ts", // Entry point of your application
-	interpreter: "/root/.bun/bin/bun",
+	interpreter: "bun",
 };
