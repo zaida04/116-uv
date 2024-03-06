b7eab2f1f148b5c773faeb35749dc60d5aa032e0
Zaid Arshad <nico.03727+github@gmail.com>
2024-03-06 14:01:42 -0500

    feat: homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+    let ubit: string = "";
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold mb-4">
+                116 Autolab Compilation Viewer
+            </h1>
+
+            <input
+                type="text"
+                placeholder="Type UBIT here"
+                class="input w-full max-w-xs mr-2"
+                bind:value={ubit}
+                on:keypress={(e) => {
+                    if (e.key === "Enter") {
+                        window.location.href = `/uploads/${ubit}`;
+                    }
+                }}
+            />
+            <a href={`/uploads/${ubit}`} class="btn btn-primary">Lookup</a>
+        </div>
+    </div>
+</div>
