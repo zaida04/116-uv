7624fa37cdd629ff209705e74292e94c8164e0d2
Zaid Arshad <nico.03727+github@gmail.com>
2024-03-25 16:48:07 -0400

    fix: docker stuff

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,8 @@ import create_upload from "./routes/create_upload";
 import get_upload from "./routes/get_request";
 import upload_exists from "./routes/get_upload";
 import get_all_uploads from "./routes/get_all_uploads";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import { db } from "./db/client";
 
 const app = new Elysia();
 app.use(cors());
@@ -32,5 +34,7 @@ app.use(get_all_uploads);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+	migrate(db, { migrationsFolder: "drizzle" });
+	console.log("Database migrated successfully");
 	console.log(`🦊 Elysia is running at ${app.server?.hostname}:${PORT}`);
 });
