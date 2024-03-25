cce73910a3e57830f3268d502ef4847521628f75
Zaid Arshad <nico.03727+github@gmail.com>
2024-03-25 16:32:53 -0400

    feat: use docker

--- api/src/db/client.ts
+++ api/src/db/client.ts
@@ -1,7 +1,7 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database("./data/sqlite.db");
 const db = drizzle(sqlite);
 
 export { db, sqlite };
